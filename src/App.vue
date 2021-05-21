a0d6da3fa136d731b7ffb9dc1ca09dc4a1dee057
Charlotte30 <2491908963@qq.com>
2021-05-21 22:59:03 +0800

    init vue wiki-v1

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1">
+    <router-link to="/" ><el-button type="primary" round>主页</el-button></router-link>
+  </el-menu-item>
+
+  <el-menu-item index="2">
+    <router-link to="/navi" ><el-button type="primary" round>导航</el-button></router-link>
+  </el-menu-item>
+
+  <el-menu-item index="3">
+      <router-link to="/todo"><el-button type="primary" round>待办</el-button></router-link>
+  </el-menu-item>
+
+  <el-menu-item index="4">
+      <router-link to="/blogs"><el-button type="primary" round>文章</el-button></router-link>
+  </el-menu-item>
+</el-menu>
+<div class="line"></div>
+
+
+  <!-- <el-container class="box-shadow">
+    <nav>
+    <router-link to="/" ><el-button type="primary" round>主页</el-button></router-link>
+    <router-link to="/navi" ><el-button type="primary" round>导航</el-button></router-link>
+    <router-link to="/todo"><el-button type="primary" round>待办</el-button></router-link>
+    <router-link to="/test"><el-button type="primary" round>文章</el-button></router-link>
+    </nav>
+  </el-container> -->
+
+  <router-view></router-view>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+<style scoped="scoped">
+    a{
+      margin: 0 0rem;
+    }
+</style>
---
+++ src/components/Vditor.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="order">
+        <h3>基于Vditor的markdown编辑器</h3>
+        <div id="vditor">test</div>
+    </div>
+</template>
+<script>
+import Vditor from "vditor"
+import "vditor/dist/index.css"
+export default {
+    data(){
+        return{
+            contentEditor:""
+        }
+    },
+    mounted(){
+        this.contentEditor = new Vditor("vditor",{
+            height:360,
+            toolbarConfig:{
+                pin:true
+            },
+            cache:{
+                enable:false
+            },
+            after:()=>{
+                this.contentEditor.setValue("hello,Vditor+Vue!")
+            }
+        })
+    },
+    methods:{
+ 
+    }
+}
+</script>
