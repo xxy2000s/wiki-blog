32e614fdf1839990131639afe4face2d245c2b14
Charlotte30 <2491908963@qq.com>
2021-05-23 15:41:50 +0800

    api-v1(lable, article) ok

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
-package wiki_server
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+	"wiki/common"
+)
+
+
+func main(){
+	InitConfig()
+	db := common.InitDB()
+	defer db.Close()
+	r := gin.New()
+	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	r.Run(":" + port)
+	//r := gin.New()
+	//r.Use(gin.Logger())
+	//r.Use(gin.Recovery())
+	//r.GET("/articles/:id", getArti)
+}
+
+func InitConfig()  {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir+"/config")
+	err := viper.ReadInConfig()
+	if err!=nil{
+		panic("读取配置错误")
+	}
+	
+}
---
+++ model/user.go
@@ -0,0 +1,9 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	Name string `gorm:"type:varchar(20);not null form:"name""`
+	Password  string `gorm:"size:255;not null" form:"password"`
+}
