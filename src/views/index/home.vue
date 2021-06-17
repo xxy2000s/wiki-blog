d43a7cc8b25db7729bf12ef4e342b2ea95f600a9
xxy2000s <xuyangxia666@gmail.com>
2021-06-17 20:40:01 +0800

    add github card && code progress

--- src/views/index/home.vue
+++ src/views/index/home.vue
@@ -103,16 +103,95 @@
       </div>
     </div>
 
-    <a href="https://github.com/xxy2000s/wiki-blog"><img src="https://gh-card.dev/repos/xxy2000s/wiki-blog.svg"></a>
+    <a href="https://github.com/xxy2000s/wiki-blog"><img class="github-card" src="https://gh-card.dev/repos/xxy2000s/wiki-blog.svg"></a>
+
+    <h2 style="text-align:center">编程语言</h2>
+    <div style="width: 500px;margin: auto;">
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="javascript"
+                   class="progress">
+        <span class="pos-left">javascript</span>
+        <span>{{javascript}}%</span>
+      </el-progress>
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="html"
+                   status="success"
+                   class="progress">
+        <span class="pos-left">html</span>
+        <span>{{html}}%</span>
+      </el-progress>
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="css"
+                   status="warning"
+                   class="progress">
+        <span class="pos-left">css</span>
+        <span>{{css}}%</span>
+      </el-progress>
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="go"
+                   class="progress">
+        <span class="pos-left">go</span>
+        <span>{{go}}%</span>
+      </el-progress>
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="python"
+                   class="progress">
+        <span class="pos-left">python</span>
+        <span>{{python}}%</span>
+      </el-progress>
+      <el-progress :text-inside="true"
+                   :stroke-width="30"
+                   :percentage="vue"
+                   class="progress">
+        <span class="pos-left">vue</span>
+        <span>{{vue}}%</span>
+      </el-progress>
+    </div>
   </div>
 
 </template>
+
+<script>
+export default {
+  data() {
+      return {
+        percentage: 10,
+        colors: [
+          {color: '#f56c6c', percentage: 20},
+          {color: '#e6a23c', percentage: 40},
+          {color: '#5cb87a', percentage: 60},
+          {color: '#1989fa', percentage: 80},
+          {color: '#6f7ad3', percentage: 100}
+        ],
+        javascript: 70,
+        html: 100,
+        css: 80,
+        go: 60,
+        python: 60,
+        vue: 70,
+      };
+    },
+}
+</script>
 
 <style scoped src="../../assets/css/feature-card.css">
 </style>
 <style scoped src="../../assets/css/poem.css">
 </style>
 <style scoped>
+.el-progress{
+    margin: 10px;
+}
+.github-card{
+    display: flex;
+    justify-content: center;
+    margin: auto;
+}
 .is-homepage {
   opacity: 0 !important;
 }
